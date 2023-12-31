21fda1988345d785b0140eaf04e5eb8425981e67
Sachin <sk7284832@gmail.com>
2023-12-31 21:59:47 +0530

    Added notes

--- Tutorial_2_variables.py
+++ Tutorial_2_variables.py
@@ -1,6 +1,3 @@
-# Variable name must starts with a letter or underscore
-# variable name can contain letters(a-z,A-Z), digits(0-9), and underscore(_)
-# variables in python are case sensative
 _4b = 10 #valid vcariable name
 print('Value of _4b is=',_4b)
 
@@ -23,4 +20,33 @@ print('value of Name is before=', Name)
 Name = 'bgruguheguighh'
 print('value of name is After=', name)
 print('value of Name is After=', Name)
+
+'''
+
+In Python, a variable is a named storage location that holds a value. Variables are used to store and manage data in a program. Here are some key points about variables in Python:
+
+
+1).Variable Assignment:
+You can create a variable and assign a value to it using the assignment operator (=). 
+
+
+2).Variable Naming Rules:
+Variable names can consist of letters (both lowercase and uppercase), numbers, and underscores.
+However, they cannot start with a number.
+Python is case-sensitive, so name and Name are different variables.
+Choose descriptive and meaningful names for your variables to improve code readability.
+
+
+3).Variable Reassignment:
+You can change the value of a variable by assigning a new value to it. For Example:-
+x = 10
+x = x + 5  # Now, x is 15
+
+
+4). Multiple Assignment:
+You can assign multiple variables in a single line. For Example:-
+a, b, c = 1, 2, 3
+
+
+'''
 
